2799fbacc6e0ed4e93a803a86570d23315f947b4
SamellR <semuele08@gmail.com>
2026-07-20 02:14:41 +0200

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'hustlecenter-v1';
+const ASSETS = [
+  './',
+  'index.html',
+  'list.html',
+  'data.json',
+  'favicon-16x16.png',
+  'favicon-32x32.png',
+  'apple-touch-icon.png',
+  'android-chrome-192x192.png',
+  'https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,400;14..32,500;14..32,600&family=Playfair+Display:ital,wght@0,600;0,700;1,600&display=swap'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).then(response => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
